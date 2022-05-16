088cdc45b869f1b01561494b109656d5e16e15f6
Youssefchahboune <chahbouney2000@outlook.fr>
2022-05-16 00:44:51 -0400

    Testing commit

--- src/main/java/com/example/c_l_flash_converter/Flash_Converter_Application.java
+++ src/main/java/com/example/c_l_flash_converter/Flash_Converter_Application.java
@@ -2,8 +2,16 @@ package com.example.c_l_flash_converter;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+
+/*
+    This is the main class where we will be building the GUI application
+
+    Step1 : Create all Classes and implement them
+    Step2 : Create the GUI
+    Step3 : Merge classes And GUI
+    Step4 : Style GUI
+ */
 
 public class Flash_Converter_Application extends Application {
 
@@ -13,7 +21,17 @@ public class Flash_Converter_Application extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        //statements
+
+        //setting window title
+        stage.setTitle("C&L Flash Converter");
+
+        //setting window size
+        stage.setMinWidth(1200);
+        stage.setMinHeight(800);
+        stage.setMaxWidth(1200);
+        stage.setMaxHeight(800);
+
+        stage.show();
     }
 
 
