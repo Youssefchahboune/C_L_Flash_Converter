9650d2031cfda780b847c959fb1cf5fa5ccb821c
Youssefchahboune <chahbouney2000@outlook.fr>
2022-05-16 00:34:53 -0400

    Initial commit

---
+++ src/main/java/com/example/c_l_flash_converter/Flash_Converter_Application.java
@@ -0,0 +1,20 @@
+package com.example.c_l_flash_converter;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Flash_Converter_Application extends Application {
+
+    public static void main(String[] args) {
+    launch();
+}
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        //statements
+    }
+
+
+}
